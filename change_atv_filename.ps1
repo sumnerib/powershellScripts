c9ff8845e41ccfb1854214c921b0f0e86d4cd213
Sumner <IBSUMNE@internal.local>
2017-11-14 10:37:44 -0500

    suppressed output from mkdir

--- change_atv_filename.ps1
+++ change_atv_filename.ps1
@@ -1,22 +1,22 @@
 function backupFiles($profile_path) {
-    mkdir $($profile_path + "backup") -ErrorAction Stop
-    Copy-Item $($profile_path + "atvantage.properties*") $($profile_path + "\backup") -ErrorAction Stop
+    mkdir $($profile_path + "backup") | Out-Null
+    Copy-Item $($profile_path + "atvantage.properties*") $($profile_path + "\backup") 
 }
 
 function renameFiles($profile_path, $old_env, $new_env) {
     backupFiles $profile_path
     backupFiles $($profile_path + $bin)
-    Rename-Item -Path $($profile_path + $atvantage) -NewName $($atvantage + "." + $old_env) -ErrorAction Stop
-    Rename-Item -Path $($profile_path + $bin + $atvantage) -NewName $($atvantage + "." + $old_env) -ErrorAction Stop
-    Rename-Item -Path $($profile_path + $atvantage + "." + $new_env) -NewName $atvantage -ErrorAction Stop
-    Rename-Item -Path $($profile_path + $bin + $atvantage + "." + $new_env) -NewName $atvantage -ErrorAction Stop
+    Rename-Item -Path $($profile_path + $atvantage) -NewName $($atvantage + "." + $old_env) 
+    Rename-Item -Path $($profile_path + $bin + $atvantage) -NewName $($atvantage + "." + $old_env) 
+    Rename-Item -Path $($profile_path + $atvantage + "." + $new_env) -NewName $atvantage 
+    Rename-Item -Path $($profile_path + $bin + $atvantage + "." + $new_env) -NewName $atvantage 
     Remove-Item $($profile_path + "backup") -recurse
     Remove-Item $($profile_path + $bin + "backup") -recurse 
 }
 
 function rollback($profile_path) {
-    Copy-Item $($profile_path + "\backup\atvantage.properties*") $profile_path -ErrorAction Stop
-    Remove-Item $($profile_path + "\backup") -recurse -ErrorAction Stop
+    Copy-Item $($profile_path + "\backup\atvantage.properties*") $profile_path 
+    Remove-Item $($profile_path + "\backup") -recurse 
 }
 
 # Switches the properties files in order to change local Atvantage or Easweb environments
@@ -54,12 +54,14 @@ if ($args[1] -eq "dev") {
     $old_env = "qa"
 }
 
+$ErrorActionPreference = "Stop"
+
 # Rename the files
 try {
     renameFiles $profile_path $old_env $args[1]
 } catch {
-    Write-Error "Rename Failed: $($PSItem.Exception.StackTrace)"
-    Write-Error $PSItem.Exception.Message
     rollback $profile_path
     rollback $($profile_path + $bin)
+    Write-Error "Rename Failed: $($PSItem.Exception.StackTrace)"
+    Write-Error $PSItem.Exception.Message
 }

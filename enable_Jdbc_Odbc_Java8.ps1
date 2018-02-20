f37ffdaea470a326ce724e30dd1ecf85105e822b
Sumner <IBSUMNE@internal.local>
2018-02-20 11:04:21 -0500

    added some fixes

--- enable_Jdbc_Odbc_Java8.ps1
+++ enable_Jdbc_Odbc_Java8.ps1
@@ -10,8 +10,27 @@ Param(
     [string]$jre
 )
 
+# Courtesy of: https://www.jonathanmedd.net/2014/02/testing-for-the-presence-of-a-registry-key-and-value.html
+function Test-RegistryValue {
+    param (
+    
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]$Path,
+        
+        [parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]$Value
+    )
+    
+    try {
+        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
+        return $true
+    } catch {
+        return $false
+    }
+}
+
 $src_path =  "\\mrkvmapp076\E\PrintAccessDB\"
-$mso_path = "HKEY_LOCAL_MACHINE\SOFTWARE\Microsoft\Office\14.0\Common\FilesPaths\"
+$mso_path = "HKLM:\SOFTWARE\Microsoft\Office\14.0\Common\FilesPaths\"
 
 # 1.  Copy jdbc.jar into 'lib/ext' folder of the JRE.
 Copy-Item $($src_path + "jdbc.jar") $($jre + "lib\ext")
@@ -35,22 +54,3 @@ if ($mso_exists) {
     Write-Host "mso.dll Removed"
 }
 Write-Host "JDBC-ODBC driver enabled!"
-
-# Courtesy of: https://www.jonathanmedd.net/2014/02/testing-for-the-presence-of-a-registry-key-and-value.html
-function Test-RegistryValue {
-    param (
-    
-        [parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]$Path,
-        
-        [parameter(Mandatory=$true)]
-        [ValidateNotNullOrEmpty()]$Value
-    )
-    
-    try {
-        Get-ItemProperty -Path $Path | Select-Object -ExpandProperty $Value -ErrorAction Stop | Out-Null
-        return $true
-    } catch {
-        return $false
-    }
-}
